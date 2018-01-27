7c225276243c2d9b2d3967f726ba6977d1054c27
gustavo <gustavo.simoes@hotmail.com.br>
2018-01-27 11:27:41 -0200

    Criação dos pacotes e entidade Pokemon e Type

---
+++ src/main/java/br/com/gustavo/pokemon/model/Pokemon.java
@@ -0,0 +1,44 @@
+package br.com.gustavo.pokemon.model;
+
+/**
+ * Created by gustavo on 27/01/18.
+ */
+public class Pokemon {
+
+    private Long id;
+
+    private String num;
+
+    private String name;
+
+    private String img;
+
+    private List<Type> type;
+
+    private String height;
+
+    private String weight;
+
+    private String candy;
+
+    private Integer candy_count;
+
+    private String egg;
+
+    private float spawn_chance;
+
+    private Integer avg_spawns;
+
+    private String spawn_time;
+
+    private List<Multiplyer> multiplyers;
+
+    private List<Weakness> weaknesses;
+
+    private List<Evolution> next_evolutions;
+
+
+
+
+
+}
---
+++ src/main/java/br/com/gustavo/pokemon/model/Type.java
@@ -0,0 +1,10 @@
+package br.com.gustavo.pokemon.model;
+
+/**
+ * Created by gustavo on 27/01/18.
+ */
+public class Type {
+
+
+
+}
